6dcdcc4c25302a30a8a48ea334c67a0f005798ad
formerFox <formerfox@yandex.ru>
2025-02-18 20:53:19 +0300

    3.3 - основные типы данных

--- ForCheck/Program.cs
+++ ForCheck/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
-string name = "Chris";
-Console.WriteLine(name);
+﻿string name = "Chris";
+byte age = 26;
+bool havingPet = false;
+int size = 38;
+Console.WriteLine($"My name - {name}");
+Console.WriteLine($"I'm {age} years");
+Console.WriteLine($"Do i have a pat? {havingPet} ");
+Console.WriteLine($"My foot size - {size}");
 Console.ReadKey();
