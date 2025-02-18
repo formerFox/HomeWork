283d9d31f54fb5734d2fcc9efb32f70a8a7968fc
formerFox <formerfox@yandex.ru>
2025-02-18 21:26:43 +0300

    3.6 - основные операции

--- ForCheck/Program.cs
+++ ForCheck/Program.cs
@@ -6,9 +6,16 @@ Console.WriteLine($"My name - {name}");
 Console.WriteLine($"I'm {age} years");
 Console.WriteLine($"Do i have a pat? {havingPet}");
 Console.WriteLine($"My foot size - {size}");
-Console.ReadKey();
-enum TrafficLightColor :int
-{ 
+//Console.ReadKey();
+
+double result = (double)5 / 2;
+Console.WriteLine("5 / 2 = {0}", result);
+
+result = 10 % 3;
+Console.WriteLine(result);
+
+enum TrafficLightColor : int
+{
     Red = 100,
     Yellow = 200,
     Green = 300
