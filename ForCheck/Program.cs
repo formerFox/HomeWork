1c53a9fbaacce15e61d3c69641fa88375b166a7f
formerFox <formerfox@yandex.ru>
2025-02-19 16:14:35 +0300

    4.1 - условные выражения и конструкции

--- ForCheck/Program.cs
+++ ForCheck/Program.cs
@@ -1,19 +1,26 @@
-﻿Console.Write("Your name - ");
-string name = Console.ReadLine();
-Console.Write("Age - ");
-byte age = (byte)int.Parse(Console.ReadLine());
-Console.WriteLine($"Your name - {name} and age is {age}");
-Console.Write("What is your favorite day of week?(number)");
-DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
-Console.WriteLine($"Your favorite day is - {day}");
+﻿Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-enum DayOfWeek : byte
+var color = Console.ReadLine();
+
+if (color == "red")
 {
-    Monday = 1,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.Black;
+
+    Console.WriteLine("Your color is red!");
+}
+
+else if (color == "green")
+{
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.ForegroundColor = ConsoleColor.Black;
+
+    Console.WriteLine("Your color is green!");
+}
+else
+{
+    Console.BackgroundColor = ConsoleColor.Cyan;
+    Console.ForegroundColor = ConsoleColor.Black;
+
+    Console.WriteLine("Your color is cyan!");
 }
