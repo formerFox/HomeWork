32ceef8c5d9a7a6a53786fd9052c4974e75921c9
formerFox <formerfox@yandex.ru>
2025-02-21 15:18:11 +0300

    4.3.16 - Массивы

--- ForCheck/Program.cs
+++ ForCheck/Program.cs
@@ -1,14 +1,10 @@
-﻿var arr = new int[10];
-Random random = new Random();
-for (int i = 0; i < arr.Length; i++)
-{
-    arr[i] = random.Next(-10, 11);
-    Console.WriteLine($"arr[{i}] = {arr[i]}");
-}
+﻿int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
 
 var count = 0;
-for (int i = 0; i < arr.Length; i++)
-{      if (arr[i] > 0)
+for (int i = 0; i <= arr.GetUpperBound(0); i++)
+    for (int j = 0; j <= arr.GetUpperBound(1); j++)
+{      if (arr[i, j] > 0)
         count++;
 }
 
