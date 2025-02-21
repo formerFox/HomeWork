03ba3b75890f0b6fd6c58275fea0d9057617a09e
formerFox <formerfox@yandex.ru>
2025-02-21 15:04:03 +0300

    4.1.12 - Массивы

--- ForCheck/Program.cs
+++ ForCheck/Program.cs
@@ -1,9 +1,15 @@
-﻿int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+﻿var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+for (int i = 0; i < arr.Length; i++)
 {
-    for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
-        Console.Write(array[k, i] + " ");
-
-    Console.WriteLine();
+    for (int j = 0; j < arr.Length; j++){ 
+        var res = 0;
+        if (arr[i] < arr[j]) {
+        res = arr[i];
+        arr[i] = arr[j];
+        arr[j] = res;
+        }
+    }
 }
+foreach (int i in arr)
+    Console.WriteLine(i);
