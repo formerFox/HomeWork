9adf358a9941f3ac4ec001b88b5b5c72553956ef
formerFox <formerfox@yandex.ru>
2025-02-21 15:06:06 +0300

    4.1.13 - Массивы

--- ForCheck/Program.cs
+++ ForCheck/Program.cs
@@ -1,15 +1,8 @@
 ﻿var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-
+var summ = 0;
 for (int i = 0; i < arr.Length; i++)
 {
-    for (int j = 0; j < arr.Length; j++){ 
-        var res = 0;
-        if (arr[i] < arr[j]) {
-        res = arr[i];
-        arr[i] = arr[j];
-        arr[j] = res;
-        }
-    }
+       summ += arr[i];
 }
-foreach (int i in arr)
-    Console.WriteLine(i);
+
+Console.WriteLine(summ);
