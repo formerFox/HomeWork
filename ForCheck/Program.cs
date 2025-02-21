cb1f34fe5faec4b102501c525c5a720f139fa91a
formerFox <formerfox@yandex.ru>
2025-02-21 14:55:11 +0300

    4.3.11 - Массивы

--- ForCheck/Program.cs
+++ ForCheck/Program.cs
@@ -1,11 +1,9 @@
-﻿Console.WriteLine("Введите имя");
-string? name = Console.ReadLine();
-for (int i = name.Length-1; i >= 0; i--)
+﻿int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
 {
-    Console.Write(name[i]+" ");
-}
-Console.WriteLine();
-foreach (char i in name)
-{
-    Console.Write(i + " ");
+    for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+        Console.Write(array[k, i] + " ");
+
+    Console.WriteLine();
 }
